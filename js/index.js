25763e5c206e553d3256fc84dbd1d8a5c78eb4d5
Eric Filser <filser.eric@protonmail.com>
2024-02-01 14:22:14 +0100

    Update fetch path

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const portfolio = document.getElementsByClassName('container')[0];
 
 // Chargez les données du fichier JSON de manière asynchrone
-fetch('../assets/datas/projects.json')
+fetch('assets/datas/projects.json')
 
   .then(response => response.json())
   .then(data => {
